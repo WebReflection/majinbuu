712b44d1a73a81390bd86e02a8f54e4f20932e7f
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2017-09-07 12:39:44 +0100

    better benchmark + removed from npm

--- benchmark.js
+++ benchmark.js
@@ -5,13 +5,14 @@ var majinbuu = require('./index.js');
 var a1 = Array(times + 1).join('.').split('.').map(Math.random);
 var a2 = a1.slice();
 var b = Array(times + 1).join('.').split('.').map(Math.random);
-
+  
 log('');
 log('# Majin Buu');
 log('size: **' + times + '**');
 console.time('time');
 majinbuu(a1, b);
 console.timeEnd('time');
+console.assert(a1.join('') === b.join(''));
 console.time('aura time');
 majinbuu(
   majinbuu.aura(
@@ -25,5 +26,13 @@ majinbuu(
   b
 );
 console.timeEnd('aura time');
-console.assert(a.join('') === b.join(''));
-log('');
+console.assert(a2.join('') === b.join(''));
+
+log('- - -');
+var crypto = require("crypto");
+var a = crypto.randomBytes(Math.ceil(times / 2)).toString('hex');
+var b = crypto.randomBytes(Math.ceil(times / 2)).toString('hex');
+console.time('C time');
+var ls = require('child_process').spawnSync('./levenstein', [a, b]);
+console.timeEnd('C time');
+log('C distance: ' + ls.stdout.toString());
